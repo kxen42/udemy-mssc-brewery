2a50245f7e263572f0651e99cd1bb07c0778680a
kxen42 <kchristenson42@mac.com>
2026-01-29 03:03:21 -0600

    Added: Complete CRUD operations for beer and customer

--- src/main/java/org/fotm/msscbrewery/web/controller/CustomerController.java
+++ src/main/java/org/fotm/msscbrewery/web/controller/CustomerController.java
@@ -1,18 +1,30 @@
 package org.fotm.msscbrewery.web.controller;
 
-import org.fotm.msscbrewery.web.model.CustomerDto;
+import java.net.URI;
 import java.util.UUID;
+import org.fotm.msscbrewery.web.model.CustomerDto;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping(value="/api/{version}/customer")
+@RequestMapping(value = "/api/{version}/customer")
 @RestController
 public class CustomerController {
 
   private final CustomerService customerService;
+  @Value("${server.address}")
+  private String serverAddress;
+  @Value("${server.port}")
+  private String serverPort;
 
   public CustomerController(CustomerService customerService) {
     this.customerService = customerService;
@@ -23,4 +35,27 @@ public class CustomerController {
     return ResponseEntity.ok(customerService.getCustomerById(customerId));
   }
 
+  @PostMapping
+  public ResponseEntity<CustomerDto> saveNewCustomer(@RequestBody CustomerDto customer,
+      @PathVariable String version) {
+
+    CustomerDto savedCustomerDto = customerService.saveNewCustomer(customer);
+    var strUri = "http://%s:%s/api/%s/customer/%s".formatted(serverAddress, serverPort, version,
+        savedCustomerDto.getId());
+    return ResponseEntity
+        .created(URI.create(strUri))
+        .body(savedCustomerDto);
+  }
+
+  @PutMapping("/{customerId}")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  public void updateCustomer(@PathVariable UUID customerId, @RequestBody CustomerDto customerDto) {
+    customerService.updateCustomer(customerId, customerDto);
+  }
+
+  @DeleteMapping("/{customerId}")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  public void deleteCustomer(@PathVariable UUID customerId) {
+    customerService.deleteCustomerById(customerId);
+  }
 }
--- src/main/java/org/fotm/msscbrewery/web/controller/CustomerService.java
+++ src/main/java/org/fotm/msscbrewery/web/controller/CustomerService.java
@@ -6,4 +6,10 @@ import java.util.UUID;
 public interface CustomerService {
 
   CustomerDto getCustomerById(UUID customerId);
+
+  CustomerDto saveNewCustomer(CustomerDto customer);
+
+  void updateCustomer(UUID customerId, CustomerDto customerDto);
+
+  void deleteCustomerById(UUID customerId);
 }
