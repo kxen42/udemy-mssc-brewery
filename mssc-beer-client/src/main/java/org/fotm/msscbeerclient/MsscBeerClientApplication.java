c1eac5e887108bd087401b4de2395aa347812447
kxen42 <kchristenson42@mac.com>
2026-01-30 17:00:09 -0600

    Added: Initialized module mssc-beer-client

---
+++ mssc-beer-client/src/main/java/org/fotm/msscbeerclient/MsscBeerClientApplication.java
@@ -0,0 +1,13 @@
+package org.fotm.msscbeerclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscBeerClientApplication {
+
+   static void main(String[] args) {
+    SpringApplication.run(MsscBeerClientApplication.class, args);
+  }
+
+}
