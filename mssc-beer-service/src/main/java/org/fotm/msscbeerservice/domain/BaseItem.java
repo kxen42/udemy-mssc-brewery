697c1148af12d5192bab5cce1f642c4969cd8032
kxen42 <kchristenson42@mac.com>
2026-02-01 23:26:02 -0600

    Added: New Beer @Entity. closes #7

---
+++ mssc-beer-service/src/main/java/org/fotm/msscbeerservice/domain/BaseItem.java
@@ -0,0 +1,31 @@
+package org.fotm.msscbeerservice.domain;
+
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+/**
+ * Using {@code OffsetDateTime} an immutable representation of a date-time with an offset from
+ * UTC/Greenwich in the ISO-8601 calendar system, such as 2007-12-03T10:15:30+01:00. In other words,
+ * it stores all date and time fields, to precision of nanoseconds, as well as the offset from
+ * GMT/UTC.
+ */
+public interface BaseItem {
+
+  UUID getId();
+
+  void setId(UUID id);
+
+  Long getVersion();
+
+  void setVersion(Long version);
+
+  OffsetDateTime getCreatedDate();
+
+  void setCreatedDate(OffsetDateTime createdDate);
+
+  OffsetDateTime getLastModifiedDate();
+
+  void setLastModifiedDate(OffsetDateTime lastModifiedDate);
+
+}
